8d59bebb8611f565f72035555870dab707ac17d6
Clarkson <fredrickotabil97@gmail.com>
2022-06-02 07:19:31 +0000

    --second commit message--

---
+++ src/slices/navSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+//selectors
+/**
+ * This is a selector that returns the origin and destination travelTimeInformation from the state.
+ * It works by using the slice name and the action name to get the value from the state.
+ */
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
